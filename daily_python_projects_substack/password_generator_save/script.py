e9fc4f31d15a575c0ea8d231e65a9580fc9d62f6
Sévak Kulinkian <s.kulinkian@gmail.com>
2025-06-14 18:33:00 +0200

    Created a "search pwd for specific account" feature in the GUI

--- daily_python_projects_substack/password_generator_save/script.py
+++ daily_python_projects_substack/password_generator_save/script.py
@@ -10,7 +10,7 @@ from manipulate_gsheet import get_sheets_service, append_data, modify_data, dele
 ###############################################
 # TODOS
 ###############################################
-# TODO Create a space in the GUI to search for an account and return nicely the password and creation date
+# TODO Implement the "gsheet current data" update advice from gemini
 
 # --LAYOUT & ELEMENTS VARIABLES--
 generated_pwd_label: ui.label = None
@@ -45,9 +45,7 @@ SPREADSHEET_ID: str = "1E7YogxyTbP4kENkD22nNk59xZBIzSe6tnh0jd2FxUhY"
 SHEET_NAME: str = "Sheet1"
 CELLS_RANGE: str = "Sheet1!A1:C"
 SERVICE = get_sheets_service()
-CURRENT_GSHEET_DATA: list = read_data(service=SERVICE, range_name=CELLS_RANGE,
-                                      spreadsheet_id=SPREADSHEET_ID)
-print(CURRENT_GSHEET_DATA)
+
 
 ###############################################
 # FUNCTIONS
@@ -129,7 +127,6 @@ def handle_generate_pwd_btn() -> None:
         state["account_name"]: str = account_spec_input.value
         state["generated_pwd"] = random_pwd_generator(state["desired_length"])
         generated_pwd_output.set_text(f"{state["generated_pwd"]}")
-        print(state["today"],state["account_name"],state["generated_pwd"] )
 
 
 def handle_save_btn_csv_version() -> None:
@@ -143,8 +140,30 @@ def handle_save_btn_gsheet_version():
     today = state["today"]
     account = state["account_name"]
     pwd = state["generated_pwd"]
-    data_to_save: list = [[today, account, pwd]] # The API expects a list of list, or a list of rows
+    data_to_save: list = [[today, account, pwd]]  # The API expects a list of list, or a list of rows
     append_data(service=SERVICE, spreadsheet_id=SPREADSHEET_ID, range_name=SHEET_NAME, data=data_to_save)
+
+
+def search_account_cred_return_pwd(searched_value: str, data: list) -> list:
+    """Look inside the gSheet and return list of save data related to searched value"""
+    all_rows_found: list = [sub_list for sub_list in data if searched_value in sub_list]
+    # In my current app it will always be the first case because I try to have unique passwords in my gSheet
+    if len(all_rows_found) == 1:
+        return all_rows_found[0]
+    else:
+        return all_rows_found
+
+
+def handle_search_pwd_btn(searched_value: str):
+    """Print in the GUI the found password for searched value, or nothing if account does not exist"""
+    pwd_found_label.set_text("")
+    current_gsheet_data: list = read_data(service=SERVICE, range_name=CELLS_RANGE,
+                                          spreadsheet_id=SPREADSHEET_ID)
+    row_found: list = search_account_cred_return_pwd(searched_value=searched_value, data=current_gsheet_data)
+    if not row_found:
+        pwd_found_label.set_text("⚠️ No password associated.")
+    else:
+        pwd_found_label.set_text(row_found[2])
 
 
 # --GENERAL GUI FUNCTIONS --
@@ -166,26 +185,42 @@ def close_app() -> None:
 # GUI LAYOUT AND RUN
 ###############################################
 with ui.column().classes("w-full h-screen flex-col justify-center items-center"):
-    with ui.column().classes(
-            "border-2 border-solid border-blue-500 rounded-md  text-lg px-10 py-10 justify-center items-center"):
-        with ui.card().classes("shadow no-border mb-4"):
-            pwd_length_label = ui.label("Password length:")
-            pwd_length_input = ui.input("Integers only")
-        with ui.card().classes("shadow no-border mb-4"):
-            account_spec_label = ui.label("Used for:")
-            account_spec_input = ui.input("Account or software name")
-        generate_pwd_btn = ui.button(text="Generate password", on_click=lambda: handle_generate_pwd_btn())
-        with ui.card().classes("shadow no-border mb-4 mt-4"):
-            generated_pwd_label = ui.label("Generated password:")
-            generated_pwd_output: ui.label = ui.label()
-        with ui.card().classes("no-shadow no-border"):
-            save_pwd_info_btn = ui.button("Save to file",
-                                          on_click=handle_save_btn_gsheet_version)
-            # ARCHIVE csv saving
-            # save_pwd_info_btn = ui.button("Save to file",
-            # on_click=handle_save_btn_csv_version)  # Here it is a var name (not a function call)
-            # because the handle_save_btn() function (see before) calls a function that will return data and store
-            # it in that variable
+    with ui.row():
+        # PASSWORD GENERATOR COLUMN
+        with ui.column().classes(
+                "border-2 border-solid border-red-500 rounded-md  text-lg px-10 py-10 justify-center items-center"):
+            with ui.card().classes("shadow no-border mb-4"):
+                pwd_length_label = ui.label("Password length:")
+                pwd_length_input = ui.input("Integers only")
+            with ui.card().classes("shadow no-border mb-4"):
+                account_spec_label = ui.label("Used for:")
+                account_spec_input = ui.input("Account or software name")
+            generate_pwd_btn = ui.button(text="Generate password", on_click=lambda: handle_generate_pwd_btn())
+            with ui.card().classes("shadow no-border mb-4 mt-4"):
+                generated_pwd_label = ui.label("Generated password:")
+                generated_pwd_output: ui.label = ui.label()
+            with ui.card().classes("no-shadow no-border"):
+                save_pwd_info_btn = ui.button("Save to file",
+                                              on_click=handle_save_btn_gsheet_version)
+                # ARCHIVE csv saving
+                # save_pwd_info_btn = ui.button("Save to file",
+                # on_click=handle_save_btn_csv_version)  # Here it is a var name (not a function call)
+                # because the handle_save_btn() function (see before) calls a function that will return data and store
+                # it in that variable
+
+        # PASSWORD SEARCHING TOOL
+        with ui.column():
+            with ui.column().classes(
+                    "border-2 border-solid border-blue-500 rounded-md  text-lg px-10 py-10 justify-center items-center"):
+                with ui.card().classes("shadow no-border mb-4"):
+                    search_account_label = ui.label("Searched account:")
+                    searched_account_input = ui.input("Type name of your account")
+                with ui.card().classes("shadow no-border mb-4"):
+                    pwd_found_title = ui.label("Password found:")
+                    pwd_found_label = ui.label()
+                generate_pwd_btn = ui.button(text="Search password", on_click=lambda: handle_search_pwd_btn(
+                    searched_value=searched_account_input.value))
+
             close_app_btn = ui.button(text="Close app", on_click=close_app).props("color=red stretch")
 
 ui.run()
